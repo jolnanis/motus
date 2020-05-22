2e657bda2d302085a57568839b4b5305e3d23322
nsirletti <n.sirletti@gmail.com>
2020-05-22 14:13:57 +0200

    Add __init__.py at root of subpackages

---
+++ dic/__init__.py
@@ -0,0 +1,5 @@
+"""
+dic: this package contains wordlists and utilities
+
+see https://docs.scrapy.org/en/latest/contributing.html#running-tests
+"""
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+"""
+tests: this package contains motus unittests
+
+see https://docs.scrapy.org/en/latest/contributing.html#running-tests
+"""
