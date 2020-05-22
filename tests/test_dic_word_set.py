2ded1df2c5d9211c4f5a2e9f99663d52bf99ad09
nsirletti <n.sirletti@gmail.com>
2020-05-22 16:55:03 +0200

    Add new tests for dic.WordSet

--- tests/test_dic_word_set.py
+++ tests/test_dic_word_set.py
@@ -1,7 +1,9 @@
-from motus.dic.WordSet import WordSet
-from motus.dic.WordSet import SubFileException
+from motus.dic.WordSet import NonAplhaWordWarning, SubFileException, WordSet
 import os
+import tempfile
 import unittest
+from warnings import catch_warnings
+
 
 SAMPLE_DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                                 'sample_data/')
@@ -32,14 +34,14 @@ class DicWordSetTest(unittest.TestCase):
         self.assertDictEqual(
             w3._substitutions,
             {'O': 'E', 'U': 'I', 'N': 'NE'},
-            )
+        )
 
         self.assertIsInstance(w4, WordSet)
         self.assertSetEqual(w4._content, set())
         self.assertDictEqual(
             w4._substitutions,
             {'O': 'E', 'U': 'I', 'N': 'NE'},
-            )
+        )
 
     def test_add_substitution(self):
         w1 = WordSet()
@@ -54,7 +56,7 @@ class DicWordSetTest(unittest.TestCase):
         self.assertDictEqual(
             w2._substitutions,
             {'O': 'I', 'U': 'I', 'N': 'NE', '$': 'S'},
-            )
+        )
 
     def test_read_substitutions(self):
         w1 = WordSet(substitution_file=SUBSTITUTION_FILE)
@@ -65,16 +67,95 @@ class DicWordSetTest(unittest.TestCase):
         self.assertDictEqual(
             w1._substitutions,
             {'O': 'E', 'U': 'I', 'N': 'NE'},
-            )
+        )
 
         w2.read_substitutions(SUBSTITUTION_FILE)
         self.assertDictEqual(
             w2._substitutions,
             {'O': 'E', 'U': 'I', 'N': 'NE'},
-            )
+        )
 
         with self.assertRaises(SubFileException):
             w3.read_substitutions(SUBSTITUTION_BROKEN_FILE)
+
+    def test_add_word(self):
+        w1 = WordSet()
+        w2 = WordSet()
+        w3 = WordSet()
+        w4 = WordSet()
+        self.assertSetEqual(w1._content, set())
+        self.assertSetEqual(w2._content, set())
+        self.assertSetEqual(w3._content, set())
+
+        w1.add('hello ')
+        self.assertSetEqual(w1._content, {'HELLO'})
+
+        w2.add_substitution('$', 'S')
+        w2.add('YE$')
+        self.assertDictEqual(w2._substitutions, {'$': 'S'})
+        self.assertSetEqual(w2._content, {'YES'})
+
+        with catch_warnings(record=True) as warnings:
+            w3.add('Hello!')
+            self.assertSetEqual(w3._content, set())
+            self.assertEqual(len(warnings), 1)
+            self.assertEqual(warnings[0].category, NonAplhaWordWarning)
+
+        with catch_warnings(record=True) as warnings:
+            w4.add('Hello World')
+            self.assertSetEqual(w4._content, set())
+            self.assertEqual(len(warnings), 1)
+            self.assertEqual(warnings[0].category, NonAplhaWordWarning)
+
+
+class TestWordSetWrite(unittest.TestCase):
+    def setUp(self):
+        self.w = WordSet()
+        self.w._content = {'SET', 'IN', 'NO', 'PARTICULAR', 'ORDER'}
+
+        tmp_handler, tmp_path = tempfile.mkstemp()
+        self.tmp_handler = tmp_handler
+        self.tmp_path = tmp_path
+
+        self.tmp_file = open(tmp_path, 'r')
+
+    def tearDown(self):
+        self.tmp_file.close()
+        os.close(self.tmp_handler)
+
+
+class TestWordSetWriteList(TestWordSetWrite):
+    def test_write(self):
+        self.w.write(self.tmp_path)
+        self.assertListEqual(
+            self.tmp_file.readlines(),
+            ['IN\n', 'NO\n', 'ORDER\n',
+                'PARTICULAR\n', 'SET\n'],
+        )
+
+
+class TestWordSetWriteDict(TestWordSetWrite):
+    def test_write_dict(self):
+        self.w.write_dict(self.tmp_path)
+        self.assertListEqual(
+            self.tmp_file.readlines(),
+            [
+                "2:\n",
+                "  I:\n",
+                "  - IN\n",
+                "  N:\n",
+                "  - 'NO'\n",       # Careful with reserved word 'NO'
+                "3:\n",
+                "  S:\n",
+                "  - SET\n",
+                "5:\n",
+                "  O:\n",
+                "  - ORDER\n",
+                "10:\n",
+                "  P:\n",
+                "  - PARTICULAR\n",
+            ],
+        )
 
 
 if __name__ == '__main__':
